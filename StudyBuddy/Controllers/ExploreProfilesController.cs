1d184931d73266d41bb266a5c319059bf7d90588
Aleksandras-2021 <79769029+Aleksandras-2021@users.noreply.github.com>
2023-09-12 22:48:17 +0300

    Feature/explore profiles (#4)
    * Added explore profiles page
    
    * Update Index.cshtml
    
    * Added ExploreProfiles section in the website
    
    -List of Users is fetched from UserManager
    -Dynamically displayed in "Explore" section of the website
    
    * Added comments
    
    * Update Index.cshtml
    
    * Minor cosmetic changes to Explore page
    
    * Small changes to profile Explorer
    
    Problem:
    When creating a new profile, the profile isn't added to the list that the ProfileExplorer fetches profiles from.
    
    * Fixed ExploreProfiles
    
    Fixed problem of new profiles not appearing on Explore page.
    
    * Update Index.cshtml
    
    * Fixed profileExplorer not loading images
    
    - Adjust ExploreProfilesController to take in IUser instead of User
    - Fixed not loading images
    - Fixed footer intersecting with images problem
    
    * Fixed a couple of goofy mistakes
    
    * Update _Layout.cshtml
    
    * Update Index.cshtml
    
    * Addressed the request for changes
    
    ---------
    
    Co-authored-by: Markas <96958120+imbieras@users.noreply.github.com>

---
+++ StudyBuddy/Controllers/ExploreProfilesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StudyBuddy.Abstractions;
+using StudyBuddy.Managers;
+using StudyBuddy.Models;
+
+namespace StudyBuddy.Controllers
+{
+    public class ExploreProfilesController : Controller
+    {
+
+        private readonly IUserManager _userManager; // Inject IUserManager
+
+        public ExploreProfilesController(IUserManager userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            try
+            {
+                List<IUser> userList = _userManager.GetAllUsers();
+                return View(userList);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+                ViewBag.ErrorMessage = "An error occurred while retrieving user profiles.";
+                return View(new List<User>()); // Provide an empty list
+            }
+        }
+
+    }
+}
