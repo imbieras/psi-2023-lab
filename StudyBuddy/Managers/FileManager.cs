24b138da008ec40adec22af12fa66ee2f7ad9a7e
Aleksandras Bukelis <aleksbukelis@gmail.com>
2023-09-19 13:11:28 +0300

    Changed CSV file reader to use CSV Helper
    Public class UserCsvRecord is kept in the same file as FileManager.cs, because it will only be used there.

--- StudyBuddy/Managers/FileManager.cs
+++ StudyBuddy/Managers/FileManager.cs
@@ -2,6 +2,8 @@
 using StudyBuddy.Abstractions;
 using StudyBuddy.Models;
 using StudyBuddy.ValueObjects;
+using CsvHelper.Configuration;
+using CsvHelper;
 
 namespace StudyBuddy.Managers
 {
@@ -21,7 +23,6 @@ namespace StudyBuddy.Managers
 
         }
 
-
         public void LoadUsersFromCsv(string filePath)
         {
             // Check if the file exists
@@ -33,44 +34,36 @@ namespace StudyBuddy.Managers
 
             try
             {
-                var csvLines = File.ReadAllLines(filePath);
-
-                // Skip the header row
-                var userDataLines = csvLines.Skip(1);
-
-                // Parse and populate users from the CSV data
-                foreach (var line in userDataLines)
+                using (var reader = new StreamReader(filePath))
+                using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)
                 {
-                    var fields = line.Split(';');
+                    Delimiter = ";", // Set the delimiter to ';'
+                }))
+                {
+                    var records = csv.GetRecords<UserCsvRecord>().ToList();
 
-                    if (fields.Length >= 6) // Make sure there are at least 6 fields
+                    foreach (var record in records)
                     {
-                        var userId = UserId.From(Guid.Parse(fields[0]));
+                        var userId = UserId.From(record.Id);
 
                         // Check if a user with the same ID already exists
                         if (_userManager.GetUserById(userId) != null)
                         {
-                            continue; // Skip this user if ID is already in UserManager
+                            continue; // Skip
                         }
 
-                        var username = fields[1];
-                        var flags = UserFlags.Registered;
-
-                        if (Enum.TryParse(fields[2], out UserFlags parsedFlags))
-                        {
-                            flags = parsedFlags;
-                        }
-
-                        var birthdate = DateTime.ParseExact(fields[3], "yyyy-MM-dd", CultureInfo.InvariantCulture);
-                        var subject = fields[4];
-                        var avatarPath = fields[5];
-
+                        var username = record.Username;
+                        var flags = Enum.TryParse(record.Flags, out UserFlags parsedFlags) ? parsedFlags : UserFlags.Registered;
+                        var birthdate = record.Birthdate;
+                        var subject = record.Subject;
+                        var avatarPath = record.AvatarPath;
                         var traits = new UserTraits(birthdate, subject, avatarPath);
+
                         _userManager.RegisterUser(username, flags, traits);
                     }
-                }
 
-                Console.WriteLine($"Loaded {userDataLines.Count()} users from the CSV file.");
+                    Console.WriteLine($"Loaded {records.Count} users from the CSV file.");
+                }
             }
             catch (Exception ex)
             {
@@ -80,4 +73,22 @@ namespace StudyBuddy.Managers
 
 
     }
+
+
+    public class UserCsvRecord
+    {
+        public Guid Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Flags { get; set; }
+
+        public DateTime Birthdate { get; set; }
+
+        public string Subject { get; set; }
+
+        public string AvatarPath { get; set; }
+    }
+
+
 }
