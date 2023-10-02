dcd26956c398e235dabf1e4251bfe13ae5a141f0
Joris <jorislisas@gmail.com>
2023-10-02 10:32:55 +0300

    added IEquatable to User

--- StudyBuddy/Models/UserModel.cs
+++ StudyBuddy/Models/UserModel.cs
@@ -3,7 +3,7 @@ using StudyBuddy.ValueObjects;
 
 namespace StudyBuddy.Models;
 
-public class User : IUser
+public class User : IUser, IEquatable<User>
 {
 
     public User(UserId id, string name, UserFlags flags, UserTraits traits)
@@ -25,4 +25,21 @@ public class User : IUser
     public static string GenerateGravatarUrl(UserId userId) =>
         // Logic to create Gravatar URL based on user's unique identifier
         $"https://www.gravatar.com/avatar/{userId}?d=identicon";
+
+    public bool Equals(User? other)
+    {
+        if (other == null) return false;
+
+        return this.Id == other.Id;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is User user && Equals(user);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
 }
