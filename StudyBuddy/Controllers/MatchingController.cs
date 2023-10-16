f30a5f9dcd5539477cb7b5d50f18595930e35f36
Aleksandras Bukelis <aleksbukelis@gmail.com>
2023-10-16 14:41:53 +0300

    Bug and formatting fixes.
    -Formatted CreateProfile.cshtml with proper indendations.
    -Birthyear can no longer be in the future
    - Match request sent message now is consistently shown, without needing to cycle through another profile
    - To make style more consistent, Go Back! button will now not dissappear, but be greyed out when it isn't allowed to be used.

--- StudyBuddy/Controllers/MatchingController.cs
+++ StudyBuddy/Controllers/MatchingController.cs
@@ -39,6 +39,7 @@ public class MatchingController : Controller
         }
 
         IUser? currentRandomUser = _userManager.GetCurrentRandomUser(currentUser);
+        ViewBag.ShowMatchRequestMessage = true;
 
         return View("RandomProfile", currentRandomUser);
     }
@@ -51,6 +52,7 @@ public class MatchingController : Controller
         if (currentUserId != null)
         {
             ViewBag.CurrentUserId = currentUserId;
+            ViewBag.ShowMatchRequestMessage = false;
         }
 
         if (!Guid.TryParse(currentUserId.ToString(), out Guid userIdGuid))
@@ -105,6 +107,7 @@ public class MatchingController : Controller
         if (currentUserId != null)
         {
             ViewBag.CurrentUserId = currentUserId;
+            ViewBag.ShowMatchRequestMessage = false;
         }
 
         if (!Guid.TryParse(currentUserId.ToString(), out Guid userIdGuid))
@@ -145,11 +148,13 @@ public class MatchingController : Controller
             {
                 // Store a "Match request sent" message in TempData
                 TempData["MatchRequestSentMessage"] = "Match request sent.";
+                ViewBag.ShowMatchRequestMessage = true;
             }
 
             if (_matchingManager.IsMatched(currentUserId, otherUserId))
             {
                 TempData["SuccessMessage"] = "Users matched successfully";
+                ViewBag.ShowMatchRequestMessage = false;
             }
         }
         catch (Exception ex)
