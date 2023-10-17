5a0b8d136433458fa1b42e9f34db168492e64997
Aleksandras Bukelis <aleksbukelis@gmail.com>
2023-10-17 14:59:17 +0300

    Update ProfileDTO.cs
    Got rid of possible null reference errors

--- StudyBuddy/Controllers/ProfileController/ProfileDTO.cs
+++ StudyBuddy/Controllers/ProfileController/ProfileDTO.cs
@@ -2,12 +2,12 @@
 
 public class ProfileDTO
 {
-    public string? Name { get; set; }
-    public string? Birthdate { get; set; }
-    public string? Subject { get; set; }
+    public string Name { get; set; }
+    public string Birthdate { get; set; }
+    public string Subject { get; set; }
     public IFormFile? Avatar { get; set; }
     public string? MarkdownContent { get; set; }
-    public List<string>? Hobbies { get; set; }
+    public List<string> Hobbies { get; set; }
     public string? Longitude { get; set; } = null;
     public string? Latitude { get; set; } = null;
 }
