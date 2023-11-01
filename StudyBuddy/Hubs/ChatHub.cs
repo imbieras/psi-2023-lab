46e29107e29607027b5e57fdcfe10f3391038fed
Aleksandras Bukelis <aleksbukelis@gmail.com>
2023-11-02 01:34:41 +0200

    Onto to something

--- StudyBuddy/Hubs/ChatHub.cs
+++ StudyBuddy/Hubs/ChatHub.cs
@@ -1,16 +1,47 @@
 ﻿using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.SignalR;
+using StudyBuddy.Managers.UserManager;
+using StudyBuddy.Services.UserService;
 
 namespace StudyBuddy.Hubs;
 
 public class ChatHub : Hub
 {
-    public async Task SendMessage(string senderId, string userId, string message)
+
+    private readonly IUserManager _userManager;
+    private readonly IUserService _userService;
+    public ChatHub(IUserManager userManager, IUserService userService)
     {
-        Console.WriteLine($"SendMessage: SenderId={senderId}, ReceiverId={userId}, Message={message}");
-        await Clients.User(userId).SendAsync("ReceiveMessage", senderId, message);
+        _userManager = userManager;
+        _userService = userService;
     }
 
+
+    public override Task OnConnectedAsync()
+    {
+        HttpContext httpContext = Context.GetHttpContext();
+
+        string receiver = httpContext.Request.Query["userid"];
+        string sender = _userService.GetCurrentUserId().ToString();
+
+        Groups.AddToGroupAsync(Context.ConnectionId, sender);
+        if (!string.IsNullOrEmpty(receiver))
+        {
+            Groups.AddToGroupAsync(Context.ConnectionId, receiver);
+        }
+
+        return base.OnConnectedAsync();
+    }
+    public Task SendMessageToGroup(string receiver, string message)
+    {
+        return Clients.Group(receiver).SendAsync("ReceiveMessage"
+            , Context.User.Identity.Name, message);
+    }
+
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
 
     public async Task ReceiveMessage(string user, string message)
     {
